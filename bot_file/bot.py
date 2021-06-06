6d5e9f19d3344a732301d569f1a8df4500ddc5fa
Mango <evan940415@gmail.com>
2021-06-06 17:50:21 +0800

    Update bot.py

--- bot_file/bot.py
+++ bot_file/bot.py
@@ -1,8 +1,6 @@
 import discord
 import json
-import os
 from discord.ext import commands
-from datetime import datetime
 
 with open('./setting/Token.json') as TokenFile:
     TokenData = json.load(TokenFile)
@@ -18,9 +16,8 @@ bot = commands.Bot(command_prefix=SettingData['Prefix'], intents=intents)
 
 @bot.event
 async def on_ready():
-    now = f'[{datetime.now().strftime("%Y/%m/%d %H:%M:%S")}]'
     BotName = SettingData['BotName']
-    print(f'{now} >> {BotName} is on ready <<')
+    print(f'>> {BotName} is on ready')
 
 @bot.command()
 async def Csetup(ctx):
@@ -28,7 +25,6 @@ async def Csetup(ctx):
         category = await ctx.guild.create_category(name=index)
         for i in channelData[index]:
             await category.create_text_channel(name=i)
-
 
 if __name__ == '__main__':
     bot.run(TokenData['Token'])
